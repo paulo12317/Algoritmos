f65c48de4065ee83c9a899cf2ee5361fa9c73886
paulo12317 <162368599+paulo12317@users.noreply.github.com>
2024-03-08 17:21:52 -0300

    Add files via upload

---
+++ exemplo/comversao.js
@@ -0,0 +1,12 @@
+const idade = 10
+const idadeTexto = idade.toString
+console.log(typeof idade)
+console.log(typeof idadeTexto)
+
+const idadeTexto2 = '15'
+const idade2 = Number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idade2)
+const numero1 = '5'
+const numero2 = '2'
+console.log(Number(numero1)+Number(numero2))
