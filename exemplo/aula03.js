844ee3b0f75b52dc4177bb2f78fa3e574d4a514c
paulo12317 <162368599+paulo12317@users.noreply.github.com>
2024-03-08 17:09:53 -0300

    Add files via upload

---
+++ exemplo/aula03.js
@@ -0,0 +1,21 @@
+//node (nome do arquivo) para execultar um comando
+console.log('eai')
+//const e let serve para criar uma variavel
+const nome = 'paulo henrique'
+let idade = 15
+console.log('meu nome é',nome,'e tenho',idade,'anos.')
+const altura = 1.75
+const temperatura = 16
+idade = 16
+console.log('meu nome é',nome,'e tenho',idade,'anos.')
+const nome1 = 'paulo'
+const sobrenome = 'vicente'
+const idade1 = 15
+const souEstudante = true
+console.log('meu',nome1,sobrenome,'e tenho',idade1,'anos e sou um estudante?',souEstudante)
+//este comando imprimi o tipo do valor
+//pode ser string, number, boolean
+console.log(typeof nome)
+const serie = 'roundsix'
+const temporadas = 1
+console.log('minha serie favorita é',serie,typeof serie,'e ela tem',temporadas,'temporada',typeof temporadas)
